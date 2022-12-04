642b2de5b7bc4923a4888f65e43d4cd3efdf92d8
mohamedahmed60 <mohamedaljabri6666@gmail.com>
2022-12-04 22:49:46 +0300

    Number even odd

---
+++ Number even odd.py
@@ -0,0 +1,23 @@
+
+"print Even Number "
+for i in range(1, 101):
+    if i%2==0:
+        print(i)
+
+print("_________________")
+
+"print Odd Number"
+for i in range(1, 101):
+    if i%3==0:
+        print(i)
+
+'''
+print('Number \t Square')
+print("-------------") 
+for i in range(1,10+1):
+    square=i*i
+    
+    print(i,'\t',square)
+   
+'''
+#type hints private documentation
